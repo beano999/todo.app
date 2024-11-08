9d937d6effd9d2b3066be6413e4f4b5b54cd28a0
beano999 <40875910+beano999@users.noreply.github.com>
2024-11-08 11:01:54 -0500

    Initial Commit - end of lab 6 or 7

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+<template>
+<header>
+    <h1>This is the header</h1>
+</header>
+</template>
+<style scoped>
+header {
+    background-color: #f0f0f0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 150px;
+}
+h1 {
+    color: #333333;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+    <body>
+        <h1>This is the main app</h1>
+    </body>
+</template>
+<style scoped></style>
